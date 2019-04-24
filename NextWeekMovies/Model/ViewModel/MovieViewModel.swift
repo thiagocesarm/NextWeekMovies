29284c54c1e453fdff4c1b13120c38f1c57bedd3
Thiago Lucena <thiagoc.lucena@gmail.com>
2019-04-24 16:50:14 -0300

    Created movie view model

---
+++ NextWeekMovies/Model/ViewModel/MovieViewModel.swift
@@ -0,0 +1,49 @@
+//
+//  MovieViewModel.swift
+//  NextWeekMovies
+//
+//  Created by Thiago Lucena on 24/04/19.
+//  Copyright © 2019 Thiago Lucena. All rights reserved.
+//
+
+import Foundation
+
+final class MovieViewModel {
+    
+    private let movie: Movie
+    private let posterImageWidth = 500
+    private let backdropImageWidth = 500
+    
+    init(movie: Movie) {
+        self.movie = movie
+    }
+    
+    var title: String {
+        return movie.title
+    }
+    
+    var posterURL: URL? {
+        if let posterPath = movie.posterPath {
+            return TMDBServices.getImage(width: posterImageWidth, imagePath: posterPath).url
+        }
+        return nil
+    }
+    
+    var backdropURL: URL? {
+        if let backdropPath = movie.backdropPath {
+            return TMDBServices.getImage(width: posterImageWidth, imagePath: backdropPath).url
+        }
+        return nil
+    }
+    
+    var genres: String {
+        let movieGenreList = GenreManager.shared.getGenreList(withIds: movie.genreIds)
+        if movieGenreList.isEmpty { return "No genres" }
+        return movieGenreList.joined(separator: ", ")
+    }
+    
+    var releaseDate: String {
+        return movie.releaseDate
+    }
+    
+}
