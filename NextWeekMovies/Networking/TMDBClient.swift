e41ca1d5fb22feecb677d8c6506e13e2c0b78cb5
Thiago Lucena <thiagoc.lucena@gmail.com>
2019-04-24 12:28:48 -0300

    Added networking client class to manage TMDB requests
    The TMDBClient class includes methods to provide access to TMDB requests. The initial requests provided are getGenresList and getUpcomingMovies (with pagination).

---
+++ NextWeekMovies/Networking/TMDBClient.swift
@@ -0,0 +1,64 @@
+//
+//  TMDBClient.swift
+//  NextWeekMovies
+//
+//  Created by Thiago Lucena on 23/04/19.
+//  Copyright © 2019 Thiago Lucena. All rights reserved.
+//
+
+import Foundation
+
+class TMDBClient {
+    
+    func getGenresList(completion: @escaping (Result<GenresFetchResult?, APIError>) -> Void ) {
+        
+        let genreService = TMDBServices.getGenresList
+        guard let url = genreService.url else {
+            preconditionFailure("Failed to construct URL")
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) {
+            data, response, error in
+            
+            DispatchQueue.main.async {
+                if let _ = error {
+                    completion(Result.failure(.apiRequestFailure))
+                } else {
+                    do {
+                        let genresResult = try JSONDecoder().decode(GenresFetchResult.self, from: data!)
+                        completion(Result.success(genresResult))
+                    } catch _ {
+                        completion(Result.failure(.jsonDecodingError))
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    func getUpcomingMoviesList(onPage page: Int, completion: @escaping (Result<MoviesFetchResult?, APIError>) -> Void) {
+        
+        let upcomingMoviesService = TMDBServices.getUpcomingMovies(page: page)
+        guard let url = upcomingMoviesService.url else {
+            preconditionFailure("Failed to construct URL")
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) {
+            data, response, error in
+            
+            DispatchQueue.main.async {
+                if let _ = error {
+                    completion(Result.failure(.apiRequestFailure))
+                } else {
+                    do {
+                        let upcomingMoviesResult = try JSONDecoder().decode(MoviesFetchResult.self, from: data!)
+                        completion(Result.success(upcomingMoviesResult))
+                    } catch _ {
+                        completion(Result.failure(.jsonDecodingError))
+                    }
+                }
+            }
+        }
+        task.resume()
+    }
+}
