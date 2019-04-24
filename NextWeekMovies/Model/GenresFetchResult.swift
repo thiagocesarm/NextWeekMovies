0affcbb959d1b0a2dd4f7ef9b3a74c07956c12d2
Thiago Lucena <thiagoc.lucena@gmail.com>
2019-04-24 12:17:20 -0300

    Added genres fetch result model
    Added model of the API response to the list genres request

---
+++ NextWeekMovies/Model/GenresFetchResult.swift
@@ -0,0 +1,13 @@
+//
+//  GenresFetchResult.swift
+//  NextWeekMovies
+//
+//  Created by Thiago Lucena on 23/04/19.
+//  Copyright © 2019 Thiago Lucena. All rights reserved.
+//
+
+import Foundation
+
+struct GenresFetchResult: Codable {
+    let genres: [Genre]
+}
